add1a9bcbf9e79e34523f26dff8a177090efa166
Claude Hähni <chaehni@student.ethz.ch>
2020-07-22 09:58:00 +0200

    multiple sizes in transfer test

--- go/sig/zoning/transfer/transfer_test.go
+++ go/sig/zoning/transfer/transfer_test.go
@@ -2,6 +2,7 @@ package transfer_test
 
 import (
 	"encoding/binary"
+	"fmt"
 	"log"
 	"net"
 	"testing"
@@ -14,51 +15,61 @@ import (
 )
 
 func BenchmarkHandleIngressSuccess(b *testing.B) {
-	size := 100000
-	n, t := setupRules(size, 1)
-	fetcher := transfer.NewMockFetcher(n, t)
-	cfg := tpconfig.TransConf{}
-	cfg.InitDefaults()
-	module := transfer.NewModule(fetcher, cfg)
-	fatal.Init()
-	transfer.Init()
-	module.StartFetcher()
+	sizes := []int{100, 1000, 10000, 100000}
+	for _, s := range sizes {
+		b.Run(fmt.Sprintf("%v subnets", s), func(b *testing.B) {
+			n, t := setupRules(s, 1)
+			fetcher := transfer.NewMockFetcher(n, t)
+			cfg := tpconfig.TransConf{}
+			cfg.InitDefaults()
+			module := transfer.NewModule(fetcher, cfg)
+			fatal.Init()
+			transfer.Init()
+			module.StartFetcher()
 
-	buf := [500]byte{}
-	srcIP := make(net.IP, 4)
-	binary.BigEndian.PutUint32(srcIP, uint32(size-1))
-	destIP := make(net.IP, 4)
-	binary.BigEndian.PutUint32(destIP, uint32(size-1))
-	pkt := zoning.Packet{Ingress: false, SrcHost: srcIP, DstHost: destIP, RawPacket: buf[:], RemoteTP: "1-ff00:0:1,127.0.0.1"}
+			buf := [500]byte{}
+			srcIP := make(net.IP, 4)
+			binary.BigEndian.PutUint32(srcIP, uint32(s-1))
+			destIP := make(net.IP, 4)
+			binary.BigEndian.PutUint32(destIP, uint32(s-1))
+			pkt := zoning.Packet{Ingress: true, SrcHost: srcIP, DstHost: destIP, RawPacket: buf[:], RemoteTP: "1-ff00:0:1,127.0.0.1"}
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_, err := module.Handle(pkt)
-		if err != nil {
-			b.Fatal(err)
-		}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				_, err := module.Handle(pkt)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
 	}
+
 }
 
 func BenchmarkHandleIngressFail(b *testing.B) {
-	n, t := setupRules(100000, 1)
-	fetcher := transfer.NewMockFetcher(n, t)
-	cfg := tpconfig.TransConf{}
-	cfg.InitDefaults()
-	module := transfer.NewModule(fetcher, cfg)
-	fatal.Init()
-	transfer.Init()
-	module.StartFetcher()
+	sizes := []int{100, 1000, 10000, 100000}
+	for _, s := range sizes {
+		b.Run(fmt.Sprintf("%v subnets", s), func(b *testing.B) {
+			n, t := setupRules(s, 1)
+			fetcher := transfer.NewMockFetcher(n, t)
+			cfg := tpconfig.TransConf{}
+			cfg.InitDefaults()
+			module := transfer.NewModule(fetcher, cfg)
+			fatal.Init()
+			transfer.Init()
+			module.StartFetcher()
 
-	buf := [500]byte{}
-	pkt := zoning.Packet{Ingress: false, SrcHost: net.IPv4(1, 0, 0, 1), DstHost: net.IPv4(0, 0, 0, 2), RawPacket: buf[:], RemoteTP: "1-ff00:0:1,127.0.0.1"}
+			buf := [500]byte{}
+			pkt := zoning.Packet{Ingress: true, SrcHost: net.IPv4(1, 1, 1, 1), DstHost: net.IPv4(0, 0, 0, 2), RawPacket: buf[:], RemoteTP: "1-ff00:0:1,127.0.0.1"}
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_, err := module.Handle(pkt)
-		if err == nil {
-			log.Fatal("expected error")
-		}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				_, err := module.Handle(pkt)
+				if err == nil {
+					log.Fatal("expected error")
+				}
+			}
+		})
 	}
 }
 
